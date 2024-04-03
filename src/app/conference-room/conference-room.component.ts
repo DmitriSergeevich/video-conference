ccca7f4d2f776d1ad0bfe1a12cfbf3ebd5d854f5
Dmitry Likhachev <valomon@yandex.ru>
2024-04-03 22:08:55 +0500

    local video implemented

--- src/app/conference-room/conference-room.component.ts
+++ src/app/conference-room/conference-room.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+const mediaConstraints = {
+    audio: true,
+    video: {
+        width: 720,
+        height: 540,
+    },
+};
 
 @Component({
     selector: 'app-conference-room',
     templateUrl: './conference-room.component.html',
     styleUrls: ['./conference-room.component.scss'],
 })
-export class ConferenceRoomComponent implements OnInit {
+export class ConferenceRoomComponent implements OnInit, AfterViewInit {
+    private localStream: MediaStream | null = null;
+    @ViewChild('local_video') localVideo!: ElementRef;
     constructor() {}
 
     ngOnInit(): void {}
+
+    ngAfterViewInit() {
+        this.requestMediaDevices();
+    }
+
+    private async requestMediaDevices(): Promise<void> {
+        this.localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+        this.localVideo.nativeElement.srcObject = this.localStream;
+    }
 }
