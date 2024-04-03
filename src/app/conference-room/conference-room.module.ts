6ca0c75680cdce2fd8343ee75de7915f64e4093d
Dmitry Likhachev <valomon@yandex.ru>
2024-04-03 21:52:13 +0500

    base markup video and actions

--- src/app/conference-room/conference-room.module.ts
+++ src/app/conference-room/conference-room.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { ConferenceRoomComponent } from './conference-room.component';
 import { ConferenceRoomRoutingModule } from './conference-room-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [ConferenceRoomComponent],
-    imports: [ConferenceRoomRoutingModule],
+    imports: [ConferenceRoomRoutingModule, MatButtonModule],
     providers: [],
     bootstrap: [AppComponent],
 })
